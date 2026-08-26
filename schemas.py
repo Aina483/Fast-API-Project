bb853e6282efaf99c2d6f3e64561963cf1ae3b51
Aina483 <ainakhanna0408@gmail.com>
2026-08-26 22:16:37 +0530

    Databasr connection - creation of models (User and post)and updation in schema

--- schemas.py
+++ schemas.py
@@ -1,14 +1,33 @@
-from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
 
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+class UserBase(BaseModel):
+    username : str = Field(min_length = 1, max_length = 50)
+    email : EmailStr =Field(max_lengyh = 120)
+
+
+
+class UserCreate(UserBase):
+    pass
+    
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id : int
+    image_file : str | None
+    image_path : str
 
 class PostBase(BaseModel):
     title : str = Field(min_length=1, max_length=100)
     content : str = Field(min_length=1)
-    author : str = Field(min_length=1, max_length=30)
+   
 
 
 class PostCreate(PostBase):
-    pass
+    user_id:int   
 
 class PostResponse(PostBase):
     # this is something that will be returned through api - not sent by client
@@ -16,4 +35,6 @@ class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
 
     id : int
-    date_posted : str
+    user_id : int
+    date_posted : datetime
+    author : UserResponse
