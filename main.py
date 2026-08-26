bfdcf1f5c41e2a2845736ef284c17b742cfdc033
Aina483 <ainakhanna0408@gmail.com>
2026-08-26 14:17:39 +0530

    Added utilities in static folder and updated the code with styling

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-from fastapi import FastAPI , Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory= "templates")
+templates = Jinja2Templates(directory="templates")
 
-users = [{
-    "name" : "Aina",
-    "age" : 23,
-    "occupation" : "Software Engineer"
-},
-{
-    "name" : "Armaan",
-    "age" : 19,
-    "occupation" : "Student"
-}
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI makes it even better.",
+        "date_posted": "April 21, 2025",
+    },
 ]
 
- 
-@app.get('/')
-def home():
-    return {"message" : "Hello world!"}
 
-@app.get('/user' , response_class=HTMLResponse)
-def get_user():
-    return f"<h1> I'm {users[0]['name']} and my age is {users[0]['age']} and I'm a {users[0]['occupation']} </h1>"
-
-@app.get('/home')
-def get_home_via_template(request : Request):
-    return templates.TemplateResponse(request, "home.html", {"users" : users})
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": posts},
+    )
 
 
+@app.get("/api/posts")
+def get_posts():
+    return posts
