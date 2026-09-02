8c8e7772e004ed390a911e1d07d9e636e1457b5a
Aina483 <ainakhanna0408@gmail.com>
2026-09-02 22:49:18 +0530

    Converting the sync code to async (creating coroutines and awaiting db operations)

--- database.py
+++ database.py
@@ -1,19 +1,46 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+# This file is for connection with the database.
+#  ----------- This is for synchronous code -----------------
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
 
-engine = create_engine(
+# ----------- for asynchronous code -------------
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase 
+
+# This is the file name that gets created automatically when the connection is build
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
+
+# engine is the connection ot the db - not immediately , it does it lazily.
+engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread" : False}
 
 )
 
-SessionLocal = sessionmaker(autocommit = False , autoflush=False, bind=engine)
+# creates db sessions - a session is basically a transaction 
+# with the database.
+# autocommit and autoflush is equal to false, because we want to control when changes are commited.
+
+# SessionLocal is a session factory
+
+#  ------------ for sync code --------
+# SessionLocal = sessionmaker(autocommit = False , autoflush=False, bind=engine)
+
+
+# ------------- for async code -------------
+AsyncSessionLocal =  async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
 
 class Base(DeclarativeBase):
     pass
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+async def get_db():
+    # with act as a context manager - whe  the block finishes , it closes the session automatically.
+    # this is dependency function that gives independent sessions to our routes 
+    with AsyncSessionLocal() as session:
+        yield session
